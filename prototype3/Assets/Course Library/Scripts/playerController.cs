fca7a63dfa108673533ddb3004cadb58f4c02f3e
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-02-07 10:47:31 -0800

    Finished 3.3
    Bug: starts with a jump

--- prototype3/Assets/Course Library/Scripts/playerController.cs
+++ prototype3/Assets/Course Library/Scripts/playerController.cs
@@ -5,24 +5,28 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10.0f;
     public float gravityModifier = 1;
     public bool isOnGround = true;
     public bool gameOver;
+    
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -35,6 +39,8 @@ public class playerController : MonoBehaviour
         } else if(Collision.gameObject.CompareTag("obstacle"))
         {
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             gameOver = true;
         }
     }
