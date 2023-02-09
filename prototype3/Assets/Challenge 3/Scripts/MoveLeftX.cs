cfc6688a1903a33a2716cfc5337fd9ee190731e2
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-02-09 12:40:42 -0800

    (almost)Finished challenge 3

--- prototype3/Assets/Challenge 3/Scripts/MoveLeftX.cs
+++ prototype3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -18,9 +18,9 @@ public class MoveLeftX : MonoBehaviour
     void Update()
     {
         // If game is not over, move to the left
-        if (playerControllerScript.gameOver)
+        if (!playerControllerScript.gameOver)
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
 
         // If object goes off screen that is NOT the background, destroy it
--- prototype3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ prototype3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public bool gameOver;
+    public bool gameOver = false;
 
-    public float floatForce;
+    public float floatForce = 1;
     private float gravityModifier = 1.5f;
     private Rigidbody playerRb;
 
@@ -16,13 +16,16 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip bounceSound;
 
 
     // Start is called before the first frame update
     void Start()
     {
         Physics.gravity *= gravityModifier;
+        playerRb = GetComponent<Rigidbody>();
         playerAudio = GetComponent<AudioSource>();
+        
 
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
@@ -33,9 +36,18 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        }
+
+        if (transform.position.y < 0)
+        {
+            playerRb.AddForce(Vector3.up * 0.5f, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        } else if (transform.position.y > 16)
+        {
+            playerRb.AddForce(Vector3.down * 0.5f, ForceMode.Impulse);
         }
     }
 
@@ -61,5 +73,7 @@ public class PlayerControllerX : MonoBehaviour
         }
 
     }
+
+    
 
 }
