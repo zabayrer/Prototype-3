ec2320af7cecca5185181534ddf1faabb4ad6383
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-02-09 12:01:04 -0800

    Finished 3.4

--- prototype3/Assets/Course Library/Scripts/playerController.cs
+++ prototype3/Assets/Course Library/Scripts/playerController.cs
@@ -10,6 +10,11 @@ public class playerController : MonoBehaviour
     public float gravityModifier = 1;
     public bool isOnGround = true;
     public bool gameOver;
+    public  ParticleSystem boomParticle;
+    public  ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     
     // Start is called before the first frame update
     void Start()
@@ -22,26 +27,35 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //jump
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio = GetComponent<AudioSource>();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
-
+    
     private void OnCollisionEnter(Collision Collision)
     {
         
         if(Collision.gameObject.CompareTag("ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if(Collision.gameObject.CompareTag("obstacle"))
         {
+            //die
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             gameOver = true;
+            boomParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
